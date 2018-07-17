9e29456abc7f68fdbc0c0f06d7bc99c2bf878f68
kenthow@gmail.com <kentah@kents-mbp.attlocal.net>
2018-07-17 13:17:40 -0500

    decoupled database info with python-decouple and added Procfile for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: has.wsgi --log-file -
--- has/settings.py
+++ has/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,8 +26,7 @@ SECRET_KEY = '+9=566r3+3-p=+-gr9el-(i#$r9n9$dg*-)&@vhm+-#lxzyar('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'has.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'huntington',
-        'USER': 'hunt',
-        'PASSWORD': 'nellie2',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
     }
 }
 
